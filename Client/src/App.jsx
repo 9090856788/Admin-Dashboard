8e08bedd187880f274d5ac3f1a0262a14e617390
9090856788 <kanhucharansahoo595@gmail.com>
2024-05-29 01:52:54 +0530

    Created the folder structure of frontend & backend application

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
 export default App;
